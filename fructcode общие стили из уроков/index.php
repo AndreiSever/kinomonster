bb4717d631ec0fe8c5b0c8b15bbf638a008b1393
Андрей Подкопаев <Apodkopaev@corp.a-r-b.ru>
2022-01-19 17:44:33 +0500

    sdasdfasdf

---
+++ fructcode общие стили из уроков/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="описание">
+        <meta name="keywords" content="html, lesson">
+        <link rel="stylesheet" src="css/style.css" media="screen and (max-width:600px)"></link>
+        
+        <title>Обучение</title>
+    </head>
+    <body>
+        <div class="wrapper">
+            <header>
+                <nav></nav>
+            </header>
+            <aside class="sidebar"></aside>
+            <aside class="ads"></aside>
+            <div class="main">
+                <section>
+                    
+                </section>
+            </div>
+            <aside class="sidebar2"></aside>
+            <footer>
+                <nav></nav>
+            </footer>
+        </div>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kinimonster fructCode</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+    <header>
+        <nav class="header-nav">
+            <div class="header-nav-elems default-container">
+                <ul>
+                    <li>
+                        <a href="" class="logo">
+                            <img src="assets/img/logo.svg" alt="">
+                        </a>
+                    </li>
+                </ul>
+                <ul class="second-menu">
+                    <li><a href="#">LIVE TV</a></li>
+                    <li><a href="#">MOVIES</a></li>
+                    <li><a href="#">SHOWS</a></li>
+                    <li><a href="#">LOG IN</a></li>
+                    <li><a href="#">REGISTER</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <main></main>
+    <footer>
+        <div class="footer-wrapper">
+            <div class="f-item">
+                <p>Main links</p>
+                <ul>
+                    <li>Movies</li>
+                    <li>Shows</li>
+                    <li>Live TV</li>
+                </ul>
+            </div>
+            <div class="f-item">
+                <p>Other links</p>
+                <ul>
+                    <li>About Us</li>
+                </ul>
+            </div>
+            <div class="f-item">
+                <p>FructCode</p>
+                <ul>
+                    <li>Our Courses</li>
+                </ul>
+            </div>
+        </div>
+        div.footer-social>p
+    </footer>
+</body>
+</html>
