e9a33fe3c97fe81390fd0489c74c5bfc7b1c740d
Андрей Подкопаев <Apodkopaev@corp.a-r-b.ru>
2022-01-20 17:51:22 +0500

    dfsdfsdfs

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
                 <ul>
                     <li>
                         <a href="" class="logo">
-                            <img src="assets/img/logo.svg" alt="">
+                            <img src="assets/img/icons/logo.svg" alt="">
                         </a>
                     </li>
                 </ul>
@@ -28,9 +28,111 @@
             </div>
         </nav>
     </header>
-    <main></main>
+    <main>
+        <section>
+            <ul>
+                <li>
+                    <img class="main-slider-img"src="assets/img/hero/fight-club-cover-fructcode_4 1.jpg" alt="">
+                    <div class="default-container">
+                        <div class="hero-movie">
+                            <p>Fight Club</p>
+                            <p>Discontented with his capitalistic lifestyle, a white-collared insomniac forms an underground fight club with Tyler</p>
+                            <button>WATCH NOW</button>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+            <div class="fader-bg">
+                <div class="fader-gradient-0"></div>
+                <div class="fader-gradient-1"></div>
+            </div>
+        </section>
+        <section class="default-container">
+            <p>On now</p>
+            <div class="posters-now">
+                <div class="poster-now">
+                    <img src="assets/img/posters/2/live-with-kelly-fructcode.jpg" alt="">
+                    <div class="postert-now-lbls">
+                        <p>S5 E2</p>
+                        <p>Live with Kelly and Ryan</p>
+                    </div>
+                </div>
+                <div class="poster-now">
+                    <img src="assets/img/posters/2/bbt-fructcode.jpg" alt="">
+                    <div class="postert-now-lbls">
+                        <p>S12 E2</p>
+                        <p>The Big Bang Theory</p>
+                    </div>
+                </div>
+                <div class="poster-now">
+                    <img src="assets/img/posters/2/s-park-fructcode.jpg" alt="">
+                    <div class="postert-now-lbls">
+                        <p>S95 E2</p>
+                        <p>South Park</p>
+                    </div>
+                </div>
+                <div class="poster-now">
+                    <img src="assets/img/posters/2/stranger-things-s-3-fructcode.jpg" alt="">
+                    <div class="postert-now-lbls">
+                        <p>S8 E6</p>
+                        <p>Stranger Things</p>
+                    </div>
+                </div>
+                <div class="poster-now">
+                    <img src="assets/img/posters/2/fclub-fructcode.jpg" alt="">
+                    <div class="postert-now-lbls">
+                        <p>MOVIE</p>
+                        <p>Fight Club</p>
+                    </div>
+                </div>
+                <div class="poster-now">
+                    <img src="assets/img/posters/2/stands-fructcode.jpg" alt="">
+                    <div class="postert-now-lbls">
+                        <p>S1 E3</p>
+                        <p>The Stand</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="default-container">
+            <p class="lbl-p">Popular Shows And Movies </p>
+            <div class="posters">
+                <div class="poster"><img src="assets/img/posters/1/stranger-things-fructcode-kinomonster.jpg" alt=""></div>
+                <div class="poster"><img src="assets/img/posters/1/friends-fructcode.jpg" alt=""></div>
+                <div class="poster"><img src="assets/img/posters/1/mand-fructcode.jpg" alt=""></div>
+                <div class="poster"><img src="assets/img/posters/1/free-guy-fructcode.jpg" alt=""></div>
+                <div class="poster"><img src="assets/img/posters/1/joker-fructcode.jpg" alt=""></div>
+                <div class="poster"><img src="assets/img/posters/1/chernobyl-fructcode.jpg" alt=""></div>
+                <div class="poster"><img src="assets/img/posters/1/space-sweepers-fructcode.jpg" alt=""></div>
+            </div>
+        </section>
+        <section class="default-container">
+            <p class="lbl-p">Recently Added Shows</p>
+            <div class="posters">
+                <div class="poster"><img src="assets/img/posters/1/shimmer-fructcode.jpg" alt=""></div>
+                <div class="poster"><img src="assets/img/posters/1/bridgerton-fructcode.jpg" alt=""></div>
+                <div class="poster"><img src="assets/img/posters/1/queens-gambit-fructcode.jpg" alt=""></div>
+                <div class="poster"><img src="assets/img/posters/1/dark-fructcode.jpg" alt=""></div>
+                <div class="poster"><img src="assets/img/posters/1/lucifer-fructcode.jpg" alt=""></div>
+                <div class="poster"><img src="assets/img/posters/1/shameless-fructcode.jpg" alt=""></div>
+                <div class="poster"><img src="assets/img/posters/1/the-undoing-fructcode.jpg" alt=""></div>
+            </div>
+        </section>
+        <section class="default-container">
+            <p class="lbl-p">Recently Added Movies</p>
+            <div class="posters">
+                <div class="poster"><img src="assets/img/posters/1/the-trial-of-chicago-fructcode.jpg" alt=""></div>
+                <div class="poster"><img src="assets/img/posters/1/the-wolf-of-wall-street-fructcode.jpg" alt=""></div>
+                <div class="poster"><img src="assets/img/posters/1/ww-84-fructcode.jpg" alt=""></div>
+                <div class="poster"><img src="assets/img/posters/1/fight-club-fructcode.jpg" alt=""></div>
+                <div class="poster"><img src="assets/img/posters/1/harry-potter-fructcode.jpg" alt=""></div>
+                <div class="poster"><img src="assets/img/posters/1/holidate-fructcode.jpg" alt=""></div>
+                <div class="poster"><img src="assets/img/posters/1/death-on-the-nile-fructcode.jpg" alt=""></div>
+            </div>
+        </section>
+    </main>
     <footer>
-        <div class="footer-wrapper">
+        <div class="footer-wrapper default-container">
             <div class="f-item">
                 <p>Main links</p>
                 <ul>
@@ -52,7 +154,19 @@
                 </ul>
             </div>
         </div>
-        div.footer-social>p
+        <div class="footer-social default-container">
+            <p>Follow us via social media:</p>
+            <a href="#"><img src="assets/img/icons/youtube.svg" alt=""></a>
+            <a href="#"><img src="assets/img/icons/facebook.svg" alt=""></a>
+            <a href="#"><img src="assets/img/icons/instagramm.svg" alt=""></a>
+            <a href="#"><img src="assets/img/icons/vk.svg" alt=""></a>
+            <a href="#"><img src="assets/img/icons/telegramm.svg" alt=""></a>
+        </div>
+        <div class="footer-copy-text">
+            <div class="default-container">
+                © FructCode.com. All rights reserved.
+            </div>
+        </div>
     </footer>
 </body>
 </html>
